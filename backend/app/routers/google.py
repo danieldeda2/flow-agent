97b071db206cafbf3cb89ece1b08ea8f443b24dd
danieldeda2 <danieldeda165@gmail.com>
2026-05-06 03:52:44 -0400

    Update google.py

--- backend/app/routers/google.py
+++ backend/app/routers/google.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_REDIRECT_URI = "http://localhost:8000/google/callback"
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost:8000/google/callback")
 GOOGLE_SCOPES = " ".join([
     "openid",
     "email",
